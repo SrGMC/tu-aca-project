8f91d00b8ffd6aae8034bb4c12bebe5d38df2546
Álvaro Galisteo <alvarogalisteo@outlook.es>
2020-01-11 00:19:13 +0100

    Fixed task3 tests

--- test/task3/agree.sh
+++ test/task3/agree.sh
@@ -3,49 +3,49 @@ mkdir results/AgreeBP/logs
 mkdir results/AgreeBP/results
 
 mkdir results/AgreeBP/results/blackscholes
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/blackscholes/blackscholes_1c_simsmall > results/AgreeBP/logs/blackscholes.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/blackscholes/blackscholes_1c_simsmall > results/AgreeBP/logs/blackscholes.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/blackscholes/
 
 mkdir results/AgreeBP/results/bodytrack
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/bodytrack/bodytrack_1c_simsmall > results/AgreeBP/logs/bodytrack.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/bodytrack/bodytrack_1c_simsmall > results/AgreeBP/logs/bodytrack.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/bodytrack/
 
 mkdir results/AgreeBP/results/canneal
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/canneal/canneal_1c_simsmall > results/AgreeBP/logs/canneal.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/canneal/canneal_1c_simsmall > results/AgreeBP/logs/canneal.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/canneal/
 
 mkdir results/AgreeBP/results/dedup
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/dedup/dedup_1c_simsmall > results/AgreeBP/logs/dedup.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/dedup/dedup_1c_simsmall > results/AgreeBP/logs/dedup.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/dedup/
 
 mkdir results/AgreeBP/results/facesim
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/facesim/facesim_1c_simsmall > results/AgreeBP/logs/facesim.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/facesim/facesim_1c_simsmall > results/AgreeBP/logs/facesim.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/facesim/
 
 mkdir results/AgreeBP/results/ferret
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/ferret/ferret_1c_simsmall > results/AgreeBP/logs/ferret.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/ferret/ferret_1c_simsmall > results/AgreeBP/logs/ferret.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/ferret/
 
 mkdir results/AgreeBP/results/fluidanimate
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/fluidanimate/fluidanimate_1c_simsmall > results/AgreeBP/logs/fluidanimate.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/fluidanimate/fluidanimate_1c_simsmall > results/AgreeBP/logs/fluidanimate.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/fluidanimate/
 
 mkdir results/AgreeBP/results/freqmine
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/freqmine/freqmine_1c_simsmall > results/AgreeBP/logs/freqmine.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/freqmine/freqmine_1c_simsmall > results/AgreeBP/logs/freqmine.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/freqmine/
 
 mkdir results/AgreeBP/results/swaptions
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/swaptions/swaptions_1c_simsmall > results/AgreeBP/logs/swaptions.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/swaptions/swaptions_1c_simsmall > results/AgreeBP/logs/swaptions.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/swaptions/
 
 mkdir results/AgreeBP/results/vips
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/vips/vips_1c_simsmall > results/AgreeBP/logs/vips.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/vips/vips_1c_simsmall > results/AgreeBP/logs/vips.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/vips/
 
 mkdir results/AgreeBP/results/x264
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/x264/x264_1c_simsmall > results/AgreeBP/logs/x264.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/x264/x264_1c_simsmall > results/AgreeBP/logs/x264.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/x264/
 
 mkdir results/AgreeBP/results/rtview
-./tu-aca-project/test/task3/run.sh AgreeBP scripts/rtview/rtview_1c_simsmall > results/AgreeBP/logs/rtview.txt
+./tu-aca-project/test/task3/run.sh AgreeBP scripts/rtview/rtview_1c_simsmall > results/AgreeBP/logs/rtview.txt 2>&1
 mv m5out/stats.txt results/AgreeBP/results/rtview/
--- test/task3/local.sh
+++ test/task3/local.sh
@@ -3,49 +3,49 @@ mkdir results/LocalBP/logs
 mkdir results/LocalBP/results
 
 mkdir results/LocalBP/results/blackscholes
-./tu-aca-project/test/task3/run.sh LocalBP scripts/blackscholes/blackscholes_1c_simsmall > results/LocalBP/logs/blackscholes.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/blackscholes/blackscholes_1c_simsmall > results/LocalBP/logs/blackscholes.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/blackscholes/
 
 mkdir results/LocalBP/results/bodytrack
-./tu-aca-project/test/task3/run.sh LocalBP scripts/bodytrack/bodytrack_1c_simsmall > results/LocalBP/logs/bodytrack.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/bodytrack/bodytrack_1c_simsmall > results/LocalBP/logs/bodytrack.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/bodytrack/
 
 mkdir results/LocalBP/results/canneal
-./tu-aca-project/test/task3/run.sh LocalBP scripts/canneal/canneal_1c_simsmall > results/LocalBP/logs/canneal.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/canneal/canneal_1c_simsmall > results/LocalBP/logs/canneal.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/canneal/
 
 mkdir results/LocalBP/results/dedup
-./tu-aca-project/test/task3/run.sh LocalBP scripts/dedup/dedup_1c_simsmall > results/LocalBP/logs/dedup.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/dedup/dedup_1c_simsmall > results/LocalBP/logs/dedup.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/dedup/
 
 mkdir results/LocalBP/results/facesim
-./tu-aca-project/test/task3/run.sh LocalBP scripts/facesim/facesim_1c_simsmall > results/LocalBP/logs/facesim.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/facesim/facesim_1c_simsmall > results/LocalBP/logs/facesim.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/facesim/
 
 mkdir results/LocalBP/results/ferret
-./tu-aca-project/test/task3/run.sh LocalBP scripts/ferret/ferret_1c_simsmall > results/LocalBP/logs/ferret.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/ferret/ferret_1c_simsmall > results/LocalBP/logs/ferret.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/ferret/
 
 mkdir results/LocalBP/results/fluidanimate
-./tu-aca-project/test/task3/run.sh LocalBP scripts/fluidanimate/fluidanimate_1c_simsmall > results/LocalBP/logs/fluidanimate.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/fluidanimate/fluidanimate_1c_simsmall > results/LocalBP/logs/fluidanimate.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/fluidanimate/
 
 mkdir results/LocalBP/results/freqmine
-./tu-aca-project/test/task3/run.sh LocalBP scripts/freqmine/freqmine_1c_simsmall > results/LocalBP/logs/freqmine.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/freqmine/freqmine_1c_simsmall > results/LocalBP/logs/freqmine.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/freqmine/
 
 mkdir results/LocalBP/results/swaptions
-./tu-aca-project/test/task3/run.sh LocalBP scripts/swaptions/swaptions_1c_simsmall > results/LocalBP/logs/swaptions.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/swaptions/swaptions_1c_simsmall > results/LocalBP/logs/swaptions.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/swaptions/
 
 mkdir results/LocalBP/results/vips
-./tu-aca-project/test/task3/run.sh LocalBP scripts/vips/vips_1c_simsmall > results/LocalBP/logs/vips.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/vips/vips_1c_simsmall > results/LocalBP/logs/vips.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/vips/
 
 mkdir results/LocalBP/results/x264
-./tu-aca-project/test/task3/run.sh LocalBP scripts/x264/x264_1c_simsmall > results/LocalBP/logs/x264.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/x264/x264_1c_simsmall > results/LocalBP/logs/x264.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/x264/
 
 mkdir results/LocalBP/results/rtview
-./tu-aca-project/test/task3/run.sh LocalBP scripts/rtview/rtview_1c_simsmall > results/LocalBP/logs/rtview.txt
+./tu-aca-project/test/task3/run.sh LocalBP scripts/rtview/rtview_1c_simsmall > results/LocalBP/logs/rtview.txt 2>&1
 mv m5out/stats.txt results/LocalBP/results/rtview/
