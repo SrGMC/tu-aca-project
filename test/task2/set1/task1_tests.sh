dc6c7f56ae510ccc712ebe8bb9896f05764b8526
Álvaro Galisteo <alvarogalisteo@outlook.es>
2020-01-11 19:32:55 +0100

    Fixed task2

--- test/task2/set1/task1_tests.sh
+++ test/task2/set1/task1_tests.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 mkdir results
-mkdir results/AgreeBP
-mkdir results/PerceptronBP
 
 # Run 1
-cp ./tu-aca-project/test/task2/set1/assets/1.py ./src/cpu/pred/BranchPredictor.py
-mkdir results/run1
-scons ./build/ALPHA/gem5.opt -j 4 > results/run1/build.txt
-./tu-aca-project/test/task2/set1/agree.sh
-./tu-aca-project/test/task2/set1/perceptron.sh
+#mkdir results/AgreeBP
+#mkdir results/PerceptronBP
+#cp ./tu-aca-project/test/task2/set1/assets/1.py ./src/cpu/pred/BranchPredictor.py
+#mkdir results/run1
+#scons ./build/ALPHA/gem5.opt -j 4 > results/run1/build.txt
+#./tu-aca-project/test/task2/set1/agree.sh
+#./tu-aca-project/test/task2/set1/perceptron.sh
 
-mv results/AgreeBP results/run1/AgreeBP
-mv results/PerceptronBP results/run1/PerceptronBP
+#mv results/AgreeBP results/run1/AgreeBP
+#mv results/PerceptronBP results/run1/PerceptronBP
 
 # Run 2
+mkdir results/AgreeBP
+mkdir results/PerceptronBP
 cp ./tu-aca-project/test/task2/set1/assets/2.py ./src/cpu/pred/BranchPredictor.py
 mkdir results/run2
 scons ./build/ALPHA/gem5.opt -j 4 > results/run2/build.txt
@@ -24,6 +26,8 @@ mv results/AgreeBP results/run2/AgreeBP
 mv results/PerceptronBP results/run2/PerceptronBP
 
 # Run 3
+mkdir results/AgreeBP
+mkdir results/PerceptronBP
 cp ./tu-aca-project/test/task2/set1/assets/3.py ./src/cpu/pred/BranchPredictor.py
 mkdir results/run3
 scons ./build/ALPHA/gem5.opt -j 4 > results/run3/build.txt
@@ -34,6 +38,8 @@ mv results/AgreeBP results/run3/AgreeBP
 mv results/PerceptronBP results/run3/PerceptronBP
 
 # Run 4
+mkdir results/AgreeBP
+mkdir results/PerceptronBP
 cp ./tu-aca-project/test/task2/set1/assets/4.py ./src/cpu/pred/BranchPredictor.py
 mkdir results/run4
 scons ./build/ALPHA/gem5.opt -j 4 > results/run4/build.txt
@@ -44,6 +50,8 @@ mv results/AgreeBP results/run4/AgreeBP
 mv results/PerceptronBP results/run4/PerceptronBP
 
 # Run 5
+mkdir results/AgreeBP
+mkdir results/PerceptronBP
 cp ./tu-aca-project/test/task2/set1/assets/5.py ./src/cpu/pred/BranchPredictor.py
 mkdir results/run5
 scons ./build/ALPHA/gem5.opt -j 4 > results/run5/build.txt
@@ -54,6 +62,8 @@ mv results/AgreeBP results/run5/AgreeBP
 mv results/PerceptronBP results/run5/PerceptronBP
 
 # Run 6
+mkdir results/AgreeBP
+mkdir results/PerceptronBP
 cp ./tu-aca-project/test/task2/set1/assets/6.py ./src/cpu/pred/BranchPredictor.py
 mkdir results/run6
 scons ./build/ALPHA/gem5.opt -j 4 > results/run6/build.txt
